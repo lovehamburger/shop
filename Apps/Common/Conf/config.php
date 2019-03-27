efbb57c78bde4f6542304d196a58c6caca6ab1c5
chenlh <243533804@qq.com>
2019-03-27 15:04:41 +0800

    git6

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -63,7 +63,7 @@ return array(
     'DATA_CACHE_COMPRESS'   =>  false,   // 数据缓存是否压缩缓存
     'DATA_CACHE_CHECK'      =>  false,   // 数据缓存是否校验缓存
     'DATA_CACHE_PREFIX'     =>  '',     // 缓存前缀
-    'DATA_CACHE_TYPE'       =>  'Db',  // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
+    'DATA_CACHE_TYPE'       =>  'File',  // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
     'DATA_CACHE_PATH'       =>  TEMP_PATH,// 缓存路径设置 (仅对File方式缓存有效)
     'DATA_CACHE_KEY'        =>  '',	// 缓存文件KEY (仅对File方式缓存有效)
     'DATA_CACHE_SUBDIR'     =>  false,    // 使用子目录缓存 (自动根据缓存标识的哈希创建子目录)
@@ -86,7 +86,8 @@ return array(
 	//SESSION设置
 	'SESSION_AUTO_START'    =>  true,    // 是否自动开启Session
     'SESSION_OPTIONS'       =>  array(), // session 配置数组 支持type name id path expire domain 等参数
-    'SESSION_TYPE'          =>  'Db', // session hander类型 默认无需设置 除非扩展了session hander驱动 Redis
+    'SESSION_TYPE'          =>  'File', // session hander类型 默认无需设置 除非扩展了session hander驱动 Redis
+    'SESSION_TYPE'          =>  'File', // session hander类型 默认无需设置 除非扩展了session hander驱动 Redis
     'SESSION_PREFIX'        =>  'sess', // session 前缀 sess_
     'REDIS_HOST' => '127.0.0.1', //REDIS服务器地址 192.168.1.241
     'REDIS_PORT' => 6379, //REDIS连接端口号 22122
