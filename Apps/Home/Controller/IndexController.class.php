a3c98e9de4ab92f7fb8b43a9be4796ffea8f333a
chenlh <243533804@qq.com>
2019-03-27 09:43:40 +0800

    git2

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -3,8 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        echo "3333";
        echo S('test111');
-       
        echo session('name');
     }
 }
