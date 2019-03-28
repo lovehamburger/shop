b3ccbae9794399f93da83071092f56241a6e309e
陈龙辉 <243533804@qq.com>
2019-03-28 14:40:19 +0800

    Update IndexController.class.php

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo "44455554";
+        echo "444555511114";
        echo S('test111');
        echo session('name');
     }
