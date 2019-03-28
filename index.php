8e5585921bdd60fdb22b3bc8685a71348c119ef3
chenlh <243533804@qq.com>
2019-03-28 09:40:54 +0800

    测试1

--- index.php
+++ index.php
@@ -12,6 +12,28 @@
 // 应用入口文件
 
 // 检测PHP环境
+$shell = "sudo git pull";
+exec($shell, $result, $status);
+var_dump($result, $status);
+
+if ($status) {
+    echo "更新命令执行失败";
+    echo'<pre>';
+    print_r($result);
+    echo'</pre>';
+    echo'<pre>';
+    print_r('状态'.$status);
+    echo'</pre>';
+} else {
+    echo "更新命令成功执行";
+    echo'<pre>';
+    print_r($result);
+    echo'</pre>';
+    echo'<pre>';
+    print_r('状态'.$status);
+    echo'</pre>';
+}
+die();
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
