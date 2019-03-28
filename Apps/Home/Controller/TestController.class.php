7dbf2f2c9557b4e85be793963da3c9225636e27a
陈龙辉 <243533804@qq.com>
2019-03-28 15:00:03 +0800

    Update TestController.class.php

--- Apps/Home/Controller/TestController.class.php
+++ Apps/Home/Controller/TestController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class TestController extends Controller {
     public function index(){
-        echo "666";
+        echo "777";
    echo  S('test111',wwww); 
    $res = M('User')->select();
 echo D()->getLastSql();
