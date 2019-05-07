452d861ba1e8950210b69ed5f04649d010ff54cd
lovehamburger <243533804@qq.com>
2019-05-07 22:08:56 +0800

    thinkphp5

--- Apps/Admin/Controller/IndexController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Admin\Controller;
-use Admin\Controller\BaseController;
-class IndexController extends BaseController {
-    public function index(){
-        $this->display();
-    }
-}
