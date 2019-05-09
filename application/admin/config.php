8a6b1e0116f80993c9a18fa6b6d41b66cc7cdfd4
chenlh <243533804@qq.com>
2019-05-09 11:37:34 +0800

    完成后台界面的搭建

--- application/admin/config.php
+++ application/admin/config.php
@@ -2,7 +2,8 @@
 
 return [
     'view_replace_str' => [
-        '__PUBLIC__'=>'/public/',
+        '__PUBLIC__'=>'/public',
+        '__PLUGINS__'=>'/public/plugins',
         '__ROOT__' => '/',
         '__SKIN__' => '/public/static/'.request()->module(),
     ],
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,4 +7,8 @@ class Index extends Base
     public function index() {
         return $this->fetch();
     }
+
+    public function home(){
+        return $this->fetch();
+    }
 }
