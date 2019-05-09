daa6f1312150aa161dd124ba2a64f38678a75f28
chenlh <243533804@qq.com>
2019-05-09 11:44:32 +0800

    修改配置文件路由

--- application/admin/config.php
+++ application/admin/config.php
@@ -3,9 +3,9 @@
 return [
     'view_replace_str' => [
         '__PUBLIC__'=>'/public',
-        '__PLUGINS__'=>'/public/plugins',
         '__ROOT__' => '/',
-        '__SKIN__' => '/public/static/'.request()->module(),
+        '__PLUGINS__'=>'/public/plugins',//插件目录
+        '__SKIN__' => '/public/static/'.request()->module(),//静态模块目录
     ],
 ]
 ?>
