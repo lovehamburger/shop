72f9bf31fb77cb1175993ea4b1064f21d1fa09ba
lovehamburger <243533804@qq.com>
2019-09-11 23:52:13 +0800

    完成类型属性的页面搭建

--- application/admin/controller/Type.php
+++ application/admin/controller/Type.php
@@ -7,6 +7,7 @@ namespace app\admin\controller;
 
 use think\Db;
 use app\common\model\Type as TypeModel;
+use app\common\model\Attr;
 use app\common\event\TypeEvent;
 use app\common\util\FilesUtil;
 
@@ -48,47 +49,6 @@ class Type extends Base
 
         $data['data'] = $TypeRes;
         return $data;
-    }
-
-    /**
-     * 修改类型状态
-     * @return array
-     */
-    public function setState() {
-        $this->_inputAjax();
-        $state = input('state/d');
-        $TypeID = input('id/d');
-        $mType = new TypeEvent();
-
-        Db::startTrans();
-        $flag = $mType->setState($TypeID, $state);
-
-        if ($flag['code'] > 0) {
-            Db::rollback();
-        } else {
-            Db::commit();
-        }
-        return $flag;
-    }
-
-    /**
-     * 设置排序
-     * @return array
-     * @throws \Exception
-     */
-    public function setSort() {
-        $this->_inputAjax();
-        $sort = json_decode_html(input('sort'));
-        Db::startTrans();
-        $mType = new TypeEvent();
-
-        $flag = $mType->setSort($sort);
-        if ($flag['code'] > 0) {
-            Db::rollback();
-        } else {
-            Db::commit();
-        }
-        return $flag;
     }
 
     /**
@@ -153,5 +113,44 @@ class Type extends Base
         $files->ext = config('uploads.goods_Type')['ext'];
         $files->thumb = config('uploads.goods_Type')['thumb'];
         return $flag = $files->uploads('image');
+    }
+
+    /************************************************************************************************************
+     *                                             类型属性
+     ***********************************************************************************************************/
+
+
+    /**
+     * 获取类型属性数据
+     * @return mixed
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getAttr() {
+        //todo权限
+        $this->_inputAjax();
+        $mAttr = new Attr();
+
+        $typeID = input('type_id/d');
+        if (empty($typeID)) {
+           return $data = array_err(9987, '请传入类型标识');
+        }
+        $param['curr_page'] = input('page/d');
+        $param['page_count'] = input('limit/d');
+        $param['type_id'] = $typeID;
+        //条数的限制
+        //dealPage();
+        $data = array_err(0, 'success');
+        $count = $mAttr->getAttrByParamCnt($param);
+        $data['count'] = $count;
+        $AttrRes = array();
+        if ($count) {
+            $field = 'id,attr_name,attr_values,type_id,attr_type,attr_type attr_type_name';
+            $AttrRes = $mAttr->getAttrByParam($param,$field);
+        }
+
+        $data['data'] = $AttrRes;
+        return $data;
     }
 }
---
+++ application/common/model/Attr.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\common\model;
+/**
+ * 商品属性模型
+ */
+class Attr extends BaseModel
+{
+
+    public function getAttrTypeNameAttr($value)
+    {
+        $attrtype = [1=>'单选属性',2=>'唯一属性'];
+        return $attrtype[$value];
+    }
+
+    /**
+     * 根据主键查询类型属性数据
+     * @param $typeID
+     * @param string $field
+     * @return Attr
+     */
+    public function getAttrByKV($typeID, $lock = false, $field = 'id,attr_name') {
+        $where['id'] = ['in', $typeID];
+        if ($lock) {
+            return $this->where($where)->master()->lock($lock)->column($field);
+        }
+        return $this->where($where)->column($field);
+    }
+
+    /**
+     * 查询类型属性数据的数量
+     * @param array $param
+     * @return int|string
+     */
+    public function getAttrByParamCnt($param = array()) {
+        $where = $this->_makeParam($param);
+        return $this->where($where)->count();
+    }
+
+    /**
+     * 获取类型属性数据
+     * @param array $param
+     * @param string $field
+     * @return false|\PDOStatement|string|\think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getAttrByParam($param = array(), $field = 'id,attr_name,attr_values,type_id') {
+        $where = $this->_makeParam($param);
+        return $this->where($where)->field($field)
+                                ->limit(($param['curr_page'] - 1) * $param['page_count'], $param['page_count'])
+                                ->select();
+    }
+
+
+    public function _makeParam($param) {
+        $where = array();
+        if (!empty($param['id'])) {
+            $where['id'] = ['in', $param['id']];
+        }
+
+        if (!empty($param['type_id'])) {
+            $where['type_id'] = ['in', $param['type_id']];
+        }
+
+        if (!empty($param['attr_name'])) {
+            $where['attr_name'] = ['LIKE', "%" . $param['attr_name'] . "%"];
+        }
+
+        return $where;
+    }
+}
