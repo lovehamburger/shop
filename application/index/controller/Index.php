840363ab91c114a0b5c34900a0cdd27bee450dbe
chenlh <243533804@qq.com>
2020-06-17 17:28:31 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,6 +6,7 @@ use app\common\model\Category;
 class Index extends Base
 {
     public function index() {
+        echo "44";
         $mCategory = new Category();
         $this->recommendCat = $mCategory->getCommendPosGoods(2,5);
         return $this->fetch();
